25e6ded8e15eb6b483d6482b4424cb2897a2c664
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2018-06-15 21:35:49 -0300

    Terminating message standarized

--- client/src/messages.py
+++ client/src/messages.py
@@ -1,4 +1,9 @@
 """ Package which hosts message types for recieving and sending """
 
 def send_text(phrase):
+    """ Format text messages and return its bytes """
     return bytes(phrase,'UTF-8')
+
+def end_connection():
+    """ Format message to terminate socket connection with server """
+    return send_text('bye')

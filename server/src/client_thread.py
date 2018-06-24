85ee30ab978a3fb5ae8c6f6ac287777511108d5b
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2018-06-23 23:06:48 -0300

    Refactored operation processing

--- server/src/client_thread.py
+++ server/src/client_thread.py
@@ -31,6 +31,49 @@ class ClientThread(threading.Thread):
         data = self.csocket.recv(2048)
         return data.decode()
 
+    def log_in_request(self, message):
+        """ Processes a login request recieved for the client.
+
+        arguments:
+            message: expected message is an array on the following format:
+                ['Log-in Request', Username, Password]
+        """
+        print(message[0])
+        username = message[1]
+        password = message[2]
+        print('\t Username: {}'.format(username))
+        print('\t Password: {}'.format(password))
+        user_exists, password_correct = database.authenticate_user(username, password)
+
+        if user_exists and password_correct:
+            print('User authenticated')
+            self.send_text('Found')
+        elif user_exists:
+            print('Authentication failure')
+            print ('Password incorrect')
+            self.send_text('Password incorrect')
+        else:
+            print('Authentication failure')
+            print('User doesnt exist')
+            self.send_text('Not found')
+
+        self.username, self.password = username, password
+
+    def register_request(self, message):
+        """ Process an registration request for the client
+
+        arguments:
+            message: expected message is an array on the following format:
+                ['Register request', Username, Password]
+        """
+        print(message[0])
+        username = message[1]
+        password = message[2]
+        print('Username: {}'.format(username))
+        print('Password: {}'.format(password))
+        database.register_user(username, password)
+        self.send_text('Created')
+
     def run(self):
         """ Main thread function
 
@@ -44,31 +87,9 @@ class ClientThread(threading.Thread):
             if msg=='bye':
                 print ("Client at ", self.caddress , " disconnected...")
                 break
-            # Wait for a operation command -----------------------------
+            # Processes an operation command -----------------------------
             split_msg = msg.split(',')
             if split_msg[0] == 'Log-in request':
-                print('Log-in request')
-                username = split_msg[1]
-                password = split_msg[2]
-                print('Username: {}'.format(username))
-                print('Password: {}'.format(password))
-                user_exists, password_correct = database.authenticate_user(username, password)
-                if user_exists and password_correct:
-                    print('User authenticated')
-                    self.send_text('Found')
-                elif user_exists:
-                    print('Authentication failure')
-                    print ('Password incorrect')
-                    self.send_text('Password incorrect')
-                else:
-                    print('Authentication failure')
-                    print('User doesnt exist')
-                    self.send_text('Not found')
+                self.log_in_request(split_msg)
             if split_msg[0] == 'Register request':
-                print('Register request')
-                username = split_msg[1]
-                password = split_msg[2]
-                print('Username: {}'.format(username))
-                print('Password: {}'.format(password))
-                database.register_user(username, password)
-                self.send_text('Created')
+                self.register_request(split_msg)

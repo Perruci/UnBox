d3f8aa205f3f43855a96ea79f95631d0f34bf7c5
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2018-06-23 22:42:55 -0300

    Avids nested pipenv environments

--- run_client.sh
+++ run_client.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-bash init_environment.sh
-
 echo "Realizando a chamada do servidor"
 python3 client/src/main.py
--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-bash init_environment.sh
-
 echo "Realizando a chamada do servidor"
 python3 server/src/main.py
